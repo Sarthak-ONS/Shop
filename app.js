d5a5b471ae72f170de7681cf6bf07db383009d2b
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2023-03-25 19:10:47 +0530

    Added pagination to index page

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  console.log(res.session, "/////////////////////////////");
+  console.log(error);
   return res.status(500).render('500', {
     pageTitle: 'Error!',
     path: '/500',
